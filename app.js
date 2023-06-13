c79190091c1877d1dfa7947e1619eff81a9af4b4
Kyle <kylepeppersack@hotmail.com>
2023-06-13 13:49:07 -0600

    Added premade rescources

---
+++ app.js
@@ -0,0 +1,86 @@
+const locations = [
+  '🎭', '🎪', '🎢', '🏟️', '🏨',
+  '🏤', '🏥', '🏭', '🏢', '🏣',
+  '🏰', '🏬', '🏥', '🏦', '🏪'
+]
+
+const people = [
+  {
+    name: 'Jimbo',
+    picture: '🤵',
+    isHunter: false,
+    location: ''
+  },
+  {
+    name: 'Sammy',
+    picture: '🙆‍♀️',
+    isHunter: false,
+    location: ''
+  },
+  {
+    name: 'Michael',
+    picture: '👷',
+    isHunter: false,
+    location: ''
+  },
+  {
+    name: 'Robert',
+    picture: '👷',
+    isHunter: false,
+    location: ''
+  },
+  {
+    name: 'Terry',
+    picture: '🤴',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Bill',
+    picture: '🕵️',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Marie',
+    picture: '👩‍🍳',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Mykeal',
+    picture: '💂',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Phil',
+    picture: '🧜‍♂️',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Wilson',
+    picture: '🏐',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Wendy',
+    picture: '👩‍⚕️',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Jeremy',
+    picture: '🦹',
+    isHunter: false,
+    location: '',
+  },
+  {
+    name: 'Mary',
+    picture: '👩‍⚖️',
+    isHunter: false,
+    location: '',
+  }
+]
